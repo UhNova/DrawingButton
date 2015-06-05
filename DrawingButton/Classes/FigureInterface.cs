0df6daceff2d2c87edc3591fae81f20664353349
FuriousCrab <20bigman94@gmail.com>
2015-06-06 01:31:03 +0700

    Рефакторинг формы

--- DrawingButton/Classes/FigureInterface.cs
+++ DrawingButton/Classes/FigureInterface.cs
@@ -4,7 +4,16 @@ namespace DrawingButton.Classes
 {
     interface FigureInterface
     {
+        /// <summary>
+        /// Метод для отрисовки фигуры
+        /// </summary>
+        /// <param name="target">Целевая фигура</param>
         void DrawFigure(Bitmap target);
+        /// <summary>
+        /// Метод перемещения начальной точки фигуры
+        /// </summary>
+        /// <param name="start">Точка начала перемещения</param>
+        /// <param name="end">Точка окончания перемещения</param>
         void MoveStart(Point start, Point end);
     }
 }
